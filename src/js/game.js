b9954e8c45c072f80ad7ec8188f740daed138514
Alexandr Yaskevich <yask.alexandr@gmail.com>
2024-04-21 16:46:03 +0300

    import second task

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+import CharacterDomain from './domain.js';
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+export class GameSavingData {
+  }
+  
+export function readGameSaving() {
+  }
+  
+export  function writeGameSaving() {
+  }
+
